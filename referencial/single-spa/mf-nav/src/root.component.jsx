6077a05d147154f4f80cbfdfd09cc6ccac720459
Vinicios Neves <vinicios.neves@mac>
2025-03-31 22:59:50 +0100

    single-spa com react 19

---
+++ referencial/single-spa/mf-nav/src/root.component.jsx
@@ -0,0 +1,14 @@
+export default function Root() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a href="/produtos">Produtos</a>
+        </li>
+        <li>
+          <a href="/clientes">Clientes</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
